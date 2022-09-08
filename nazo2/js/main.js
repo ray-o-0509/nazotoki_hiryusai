e2e25faa15a8d30d40bca7ccc7332f95e513354c
Ray Otsuka <56874556+ray-o-0509@users.noreply.github.com>
2022-09-09 02:48:12 +0900

    [add] added nazotoki

---
+++ nazo2/js/main.js
@@ -0,0 +1,26 @@
+let pw_text = '';
+
+function clicedNum(num) {
+    pw_text += num;
+    $('.pw_text').text(pw_text);
+    if (pw_text.length == 4) {
+        if (pw_text == '8154') {
+            $('.enter_num_section').hide();
+            $('.correct_div').css('display', 'flex');
+            setTimeout(resetAll, 3000);
+        } else {
+            $('.enter_num_section').hide();
+            $('.wrong_div').css('display', 'flex');
+            setTimeout(resetAll, 1000);
+
+        }
+    }
+}
+
+function resetAll() {
+    pw_text = '';
+    $('.pw_text').text('xxxx');
+    $('.enter_num_section').show();
+    $('.correct_div').css('display', 'none');
+    $('.wrong_div').css('display', 'none');
+}
